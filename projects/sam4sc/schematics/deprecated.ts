81d24e9d59306a2df1602a610fbb00f5d29aacf2
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-02-11 16:29:30 +0100

    add deprecation message for old lib name

---
+++ projects/sam4sc/schematics/deprecated.ts
@@ -0,0 +1,12 @@
+console.log(`
+Dear user,
+
+this schematic has been renamed to @angular-architects/scam4sc.
+
+Please install it via: "npm install -D @angular-architects/scam4sc".
+
+Then execute the generators via "ng generate @angular-architects/scam4sc:scam"
+
+All the best,
+Rainer
+`);
